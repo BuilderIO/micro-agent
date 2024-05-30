b0f3f1ea7f8281b6d666ec046524cdc1ea780c4c
Steve Sewell <steve@builder.io>
2024-05-30 08:10:50 -0700

    updates

--- test/nextjs-app/app/page.tsx
+++ test/nextjs-app/app/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
-export default function Home() {
+const Home: React.FC = () => {
   return <main className="bg-white min-h-screen py-12 px-4 sm:px-8"></main>;
-}
+};
+
+export default Home;
