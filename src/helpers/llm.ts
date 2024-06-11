c3b908bb8dbb0bd3fb7e2c0d11a2d99df197a68d
Manu MA <manu.mtza@gmail.com>
2024-06-11 17:07:44 +0200

    🐓

--- src/helpers/llm.ts
+++ src/helpers/llm.ts
@@ -24,7 +24,6 @@ const useOllama = (model?: string) => {
 };
 
 const supportsFunctionCalling = (model?: string) => {
-  // gpt-4o, gpt-4o-2024-05-13, gpt-4-turbo, gpt-4-turbo-2024-04-09, gpt-4-turbo-preview, gpt-4-0125-preview, gpt-4-1106-preview, gpt-4, gpt-4-0613, gpt-3.5-turbo, gpt-3.5-turbo-0125, gpt-3.5-turbo-1106, and gpt-3.5-turbo-0613
   return !!{
     'gpt-4o': true,
     'gpt-4o-2024-05-13': true,
