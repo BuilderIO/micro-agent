1ab4482d41d1d7b4f3631bff2a71bfcf42425b61
Steve Sewell <steve@builder.io>
2024-06-12 11:28:25 -0700

    better match the coding style

---
+++ src/helpers/find-package-json.ts
@@ -0,0 +1,21 @@
+import * as fs from 'fs/promises';
+import * as path from 'path';
+import { fileExists } from './file-exists';
+
+/**
+ * Find package.json file in the given directory or any
+ * parent directory.
+ */
+export async function findPackageJson(
+  directory: string
+): Promise<string | null> {
+  let currentDirectory = directory;
+  while (currentDirectory !== '/') {
+    const packageJsonPath = path.join(currentDirectory, 'package.json');
+    if (await fileExists(packageJsonPath)) {
+      return fs.readFile(packageJsonPath, 'utf-8');
+    }
+    currentDirectory = path.dirname(currentDirectory);
+  }
+  return null;
+}
