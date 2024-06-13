9e450c9f13be837b0a271aac5b2f6a427a317080
Scott Boyd <scott.boyd.ai@gmail.com>
2024-06-12 18:27:27 -0600

    Prettier

--- src/tests/integration/cli.test.ts
+++ src/tests/integration/cli.test.ts
@@ -42,29 +42,32 @@ describe('cli', () => {
   });
 
   it('should work on spec file', async () => {
-  await writeFile(`${integrationTestPath}/add.test.ts`, 'import { test, expect } from \'vitest\';\n\ntest(\'pass\', () => {\nexpect(true)\n});');
+    await writeFile(
+      `${integrationTestPath}/add.test.ts`,
+      "import { test, expect } from 'vitest';\n\ntest('pass', () => {\nexpect(true)\n});"
+    );
 
     // Write the test file using the mock LLM record
     const mockLlmRecordFile = 'test/fixtures/add.json';
     const mockLlmRecordFileContents = await readFile(
-        mockLlmRecordFile,
-        'utf-8'
+      mockLlmRecordFile,
+      'utf-8'
     );
     const jsonLlmRecording = JSON.parse(mockLlmRecordFileContents.toString());
 
     const testContents = jsonLlmRecording.completions[1].output;
     await writeFile(
-        `${integrationTestPath}/add.spec.ts`,
-        removeBackticks(testContents)
+      `${integrationTestPath}/add.spec.ts`,
+      removeBackticks(testContents)
     );
 
     // Execute the CLI command
     const result = await execaCommand(
-        `USE_MOCK_LLM=true MOCK_LLM_RECORD_FILE=test/fixtures/add.json jiti ./src/cli.ts ${integrationTestPath}/add.ts -f ${integrationTestPath}/add.spec.ts -t "npm run test:all -- add"`,
-        {
-          input: '\x03',
-          shell: process.env.SHELL || true,
-        }
+      `USE_MOCK_LLM=true MOCK_LLM_RECORD_FILE=test/fixtures/add.json jiti ./src/cli.ts ${integrationTestPath}/add.ts -f ${integrationTestPath}/add.spec.ts -t "npm run test:all -- add"`,
+      {
+        input: '\x03',
+        shell: process.env.SHELL || true,
+      }
     );
 
     const output = result.stdout;
@@ -78,9 +81,12 @@ describe('cli', () => {
     expect(output).toContain('All tests passed!');
   });
 
-    afterEach(async () => {
-        await writeFile(`${integrationTestPath}/add.ts`, '');
-        await writeFile(`${integrationTestPath}/add.test.ts`, '');
-        await writeFile(`${integrationTestPath}/add.spec.ts`, 'import {describe, test} from \'vitest\'\n describe(\'spec\', () => {test.todo(\'please pass\');});');
-    })
+  afterEach(async () => {
+    await writeFile(`${integrationTestPath}/add.ts`, '');
+    await writeFile(`${integrationTestPath}/add.test.ts`, '');
+    await writeFile(
+      `${integrationTestPath}/add.spec.ts`,
+      "import {describe, test} from 'vitest'\n describe('spec', () => {test.todo('please pass');});"
+    );
+  });
 });
