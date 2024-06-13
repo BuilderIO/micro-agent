fe14d25d9a4c3971735562fdf189333ecadef52c
Vishwas <vishwas.g5@gmail.com>
2024-06-13 14:18:43 +0530

    Update get-test-command prompt

--- src/helpers/get-test-command.ts
+++ src/helpers/get-test-command.ts
@@ -28,16 +28,27 @@ export async function getTestCommand({
         {
           role: 'user',
           content: dedent`
-            Here is my package.json. I want to run a single command to run the tests (e.g. "npm run test"). The tests should not run in watch mode, they should
-            exit once completed. Bonus points if the command only runs this specific test \`${testFilePath}\` such as \`npm test -- ${
+            Here is my package.json. I want to run a single command to execute the tests. The tests should not run in watch mode.
+            If there is a test script in the package.json, use that script. For example, \`npm test\`.
+            The command should filter and run the specific test file at \`${testFilePath}\`. For example, \`npm test -- ${
             testFilePath.split('/').pop()!.split('.')[0]
           }\`.
+
+            Here are sample npm test commands without watch mode that work for some popular testing libraries:
+            - Jest: \`npm test -- ${
+              testFilePath.split('/').pop()!.split('.')[0]
+            } --no-watch \`
+            - Vitest: \`npm test -- ${
+              testFilePath.split('/').pop()!.split('.')[0]
+            } --run \`
 
             <package-json>
             ${packageJson}
             </package-json>
 
-            If there are no testing libraryes in the package.json, \`npx vitest ...\` is a good option.
+            If no testing libraries are found in the package.json, use \`npx vitest run ${
+              testFilePath.split('/').pop()!.split('.')[0]
+            }\` as a fallback.
           `,
         },
       ],
