14467766f80366976b6d82182ce0351dadaa0adb
Paul Shippy <paul.shippy@strongmind.com>
2024-06-22 16:55:27 -0700

    Support python, ruby, and javascript dependency files

--- src/helpers/dependency-files.test.ts
+++ src/helpers/dependency-files.test.ts
@@ -1,9 +1,10 @@
 import { describe, it, expect, vi } from 'vitest';
-import { getDependenciesFileContent } from './dependency-files';
+import { getDependencyFile, getDependencyFileName } from './dependency-files';
 
 const mocks = vi.hoisted(() => {
   return {
     readFile: vi.fn(),
+    fileExists: vi.fn(),
   };
 });
 
@@ -13,55 +14,89 @@ vi.mock('fs/promises', () => {
   };
 });
 
-describe('getDependenciesFileContent', () => {
+vi.mock('./file-exists', () => {
+  return {
+    fileExists: mocks.fileExists,
+  };
+});
+
+describe('getDependencyFile', () => {
   it('should return the contents of package.json for node', async () => {
     const packageJsonContent = JSON.stringify({
       name: 'example',
       version: '1.0.0',
     });
     mocks.readFile.mockResolvedValueOnce(packageJsonContent);
+    mocks.fileExists.mockResolvedValue(true);
 
-    const result = await getDependenciesFileContent();
+    const result = await getDependencyFile('', 'ts');
     expect(result).toBe(packageJsonContent);
+    expect(mocks.readFile).toHaveBeenCalledWith('package.json', 'utf8');
   });
 
   it('should return the contents of requirements.txt for python', async () => {
     const requirementsTxtContent = 'example-package==1.0.0';
     mocks.readFile.mockResolvedValueOnce(requirementsTxtContent);
+    mocks.fileExists.mockResolvedValue(true);
 
-    const result = await getDependenciesFileContent('py');
+    const result = await getDependencyFile('', 'py');
     expect(result).toBe(requirementsTxtContent);
+    expect(mocks.readFile).toHaveBeenCalledWith('requirements.txt', 'utf8');
   });
 
   it('should return the contents of Gemfile for ruby', async () => {
     const gemfileContent = "gem 'rails', '5.0.0'";
     mocks.readFile.mockResolvedValueOnce(gemfileContent);
+    mocks.fileExists.mockResolvedValue(true);
 
-    const result = await getDependenciesFileContent('rb');
+    const result = await getDependencyFile('', 'rb');
     expect(result).toBe(gemfileContent);
+    expect(mocks.readFile).toHaveBeenCalledWith('Gemfile', 'utf8');
   });
 
-  it('should throw an error if package.json does not exist for node', async () => {
-    mocks.readFile.mockRejectedValueOnce(new Error('File not found'));
+  it('should check all three dependency files if no language is provided', async () => {
+    mocks.fileExists.mockReset();
+    mocks.fileExists.mockResolvedValue(false);
 
-    await expect(getDependenciesFileContent()).rejects.toThrow(
-      'File not found'
-    );
+    const result = await getDependencyFile('/src');
+    expect(mocks.fileExists).toHaveBeenCalledTimes(3);
+    expect(mocks.fileExists).toHaveBeenCalledWith('/src/package.json');
+    expect(mocks.fileExists).toHaveBeenCalledWith('/src/requirements.txt');
+    expect(mocks.fileExists).toHaveBeenCalledWith('/src/Gemfile');
   });
 
-  it('should throw an error if requirements.txt does not exist for python', async () => {
-    mocks.readFile.mockRejectedValueOnce(new Error('File not found'));
+  it('should return null if package.json file does not exist', async () => {
+    mocks.fileExists.mockResolvedValue(false);
 
-    await expect(getDependenciesFileContent('py')).rejects.toThrow(
-      'File not found'
-    );
+    await expect(getDependencyFile()).resolves.toBeNull();
   });
 
-  it('should throw an error if Gemfile does not exist for ruby', async () => {
-    mocks.readFile.mockRejectedValueOnce(new Error('File not found'));
+  it('should return null if requirements.txt does not exist for python', async () => {
+    mocks.fileExists.mockResolvedValue(false);
 
-    await expect(getDependenciesFileContent('rb')).rejects.toThrow(
-      'File not found'
-    );
+    await expect(getDependencyFile(process.cwd(), 'py')).resolves.toBeNull();
+  });
+
+  it('should return null if Gemfile does not exist for ruby', async () => {
+    mocks.fileExists.mockResolvedValue(false);
+
+    await expect(getDependencyFile(process.cwd(), 'rb')).resolves.toBeNull();
+  });
+});
+
+describe('getDependencyFileName', () => {
+  it('should return package.json for node', () => {
+    const result = getDependencyFileName();
+    expect(result).toBe('package.json');
+  });
+
+  it('should return requirements.txt for python', () => {
+    const result = getDependencyFileName('py');
+    expect(result).toBe('requirements.txt');
+  });
+
+  it('should return Gemfile for ruby', () => {
+    const result = getDependencyFileName('rb');
+    expect(result).toBe('Gemfile');
   });
 });
--- src/helpers/dependency-files.ts
+++ src/helpers/dependency-files.ts
@@ -1,8 +1,43 @@
-import { readFile } from 'fs/promises';
+import * as fs from 'fs/promises';
+import * as path from 'path';
+import { fileExists } from './file-exists';
 
-export async function getDependenciesFileContent(
+/**
+ * Find dependency file in the given directory or any
+ * parent directory. Returns the content of the dependency file.
+ */
+export async function getDependencyFile(
+  directory = process.cwd(),
   language?: string
-): Promise<string> {
+): Promise<string | null> {
+  let currentDirectory = directory;
+  while (currentDirectory !== '/') {
+    if (language) {
+      const filePath = getDependenciesFilePath(directory, language);
+
+      if (await fileExists(filePath)) {
+        return getDependenciesFileContent(directory, language);
+      }
+    } else {
+      let filePath = getDependenciesFilePath(directory, 'py');
+      if (await fileExists(filePath)) {
+        return getDependenciesFileContent(directory, 'py');
+      }
+      filePath = getDependenciesFilePath(directory, 'rb');
+      if (await fileExists(filePath)) {
+        return getDependenciesFileContent(directory, 'rb');
+      }
+      filePath = getDependenciesFilePath(directory, 'js');
+      if (await fileExists(filePath)) {
+        return getDependenciesFileContent(directory, language);
+      }
+    }
+    currentDirectory = path.dirname(currentDirectory);
+  }
+  return null;
+}
+
+export function getDependencyFileName(language?: string): string {
   let fileName;
   switch (language) {
     case 'py':
@@ -15,7 +50,19 @@ export async function getDependenciesFileContent(
       fileName = 'package.json';
       break;
   }
+  return fileName;
+}
 
-  const fileContent = await readFile(fileName, 'utf8');
+function getDependenciesFilePath(directory: string, language?: string): string {
+  const fileName = getDependencyFileName(language);
+  return path.join(directory, fileName);
+}
+
+async function getDependenciesFileContent(
+  directory = process.cwd(),
+  language?: string
+): Promise<string> {
+  const filePath = getDependenciesFilePath(directory, language);
+  const fileContent = await fs.readFile(filePath, 'utf8');
   return fileContent;
 }
